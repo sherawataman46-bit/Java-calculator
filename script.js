311492a15a33be33d7329981597740c7e9bde4e6
Aman <sherawataman46@gmail.com>
2026-03-06 14:47:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function appendValue(value){
+document.getElementById("display").value += value;
+}
+
+function calculate(){
+let result = eval(document.getElementById("display").value);
+document.getElementById("display").value = result;
+}
+
+function clearDisplay(){
+document.getElementById("display").value = "";
+}
